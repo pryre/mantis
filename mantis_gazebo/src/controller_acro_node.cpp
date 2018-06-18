3be5522277c3fa6a8c9bfce38a1f4a43d64e4085
Kye Morton <pryre.dev@outlook.com>
2018-06-19 01:16:42 +1000

    updated acro sim to work with aug lite

--- mantis_gazebo/src/controller_acro_node.cpp
+++ mantis_gazebo/src/controller_acro_node.cpp
@@ -4,10 +4,11 @@
 #include <tf2/LinearMath/Matrix3x3.h>
 
 #include <mavros_msgs/AttitudeTarget.h>
-#include <mavros_msgs/RCOut.h>
+#include <mavros_msgs/ActuatorControl.h>
+#include <mavros_msgs/OverrideRCIn.h>
 #include <sensor_msgs/Imu.h>
 
-#include <pidController/pidController.h>
+#include <pid_controller_lib/pidController.h>
 
 #include <string>
 
@@ -28,25 +29,23 @@ typedef struct controlCommand_s {
 } controlCommand_t;
 
 const static motorMixer_t mixer_hexacopter_x[NUM_MOTORS] = {
-	{ 1.0,	-1.0,	 0.0,		 1.0}, // Motor 1
-	{ 1.0,	 1.0,	 0.0,		-1.0}, // Motor 2
-	{ 1.0,	 0.5,	-0.866025,	 1.0}, // Motor 3
-	{ 1.0,	-0.5,	 0.866025,	-1.0}, // Motor 4
-	{ 1.0,	-0.5,	-0.866025,	-1.0}, // Motor 5
-	{ 1.0,	 0.5,	 0.866025,	 1.0}, // Motor 6
+	{ 1.0,	-1.0,	 0.0,	 1.0}, // Motor 1
+	{ 1.0,	 1.0,	 0.0,	-1.0}, // Motor 2
+	{ 1.0,	 0.5,	-1.0,	 1.0}, // Motor 3
+	{ 1.0,	-0.5,	 1.0,	-1.0}, // Motor 4
+	{ 1.0,	-0.5,	-1.0,	-1.0}, // Motor 5
+	{ 1.0,	 0.5,	 1.0,	 1.0}, // Motor 6
 };
 
 class ControllerAcro {
 	private:
 		ros::NodeHandle nh_;
+		ros::NodeHandle nhp_;
 
 		ros::Subscriber sub_attitude_target_;
+		ros::Subscriber sub_actuator_control_;
 		ros::Subscriber sub_imu_;
 		ros::Publisher pub_rc_out_;
-
-		std::string topic_attitude_target_;
-		std::string topic_imu_;
-		std::string topic_rc_out_;
 
 		std::string model_name_;
 		sensor_msgs::Imu model_imu_;
@@ -54,9 +53,11 @@ class ControllerAcro {
 		ros::Timer tmr_rc_out_;
 		double pwm_update_rate_;
 		std::string frame_id_;
-		mavros_msgs::RCOut msg_rc_out_;
+
+		double force_comp_timeout_;
 
 		mavros_msgs::AttitudeTarget goal_att_;
+		mavros_msgs::ActuatorControl force_compensation_;
 
 		double param_ang_r_ff_;
 		double param_ang_p_ff_;
@@ -82,7 +83,6 @@ class ControllerAcro {
 		}
 
 		void mixerCallback(const ros::TimerEvent& event) {
-			msg_rc_out_.header.stamp = event.current_real;
 			double dt = (event.current_real - event.last_real).toSec();
 
 			if(!( goal_att_.type_mask & goal_att_.IGNORE_THRUST)) {
@@ -145,6 +145,15 @@ class ControllerAcro {
 			control_forces_.p = controller_rates_y_.step( dt, control_rates_.p, model_imu_.angular_velocity.y );
 			control_forces_.y = controller_rates_z_.step( dt, control_rates_.y, model_imu_.angular_velocity.z );
 
+			//Check to see if the force compensation message is still recent
+			if( (event.current_real - force_compensation_.header.stamp).toSec() < force_comp_timeout_) {
+				//Add in the force compensation
+				control_forces_.r += force_compensation_.controls[0];
+				control_forces_.p += force_compensation_.controls[1];
+				control_forces_.y += force_compensation_.controls[2];
+			}
+
+			//Perform the PWM mapping
 			int32_t max_output = 1000;
 			int32_t scale_factor = 1000;
 			int32_t prescaled_outputs[NUM_MOTORS];
@@ -174,16 +183,22 @@ class ControllerAcro {
 
 			scale_factor = 1000;
 
+			mavros_msgs::OverrideRCIn msg_rc_out;
+
 			for (uint8_t i=0; i<NUM_MOTORS; i++) {
 					 int32_t channel_out = (prescaled_outputs[i] * scale_factor / 1000); // divide by scale factor
-					 msg_rc_out_.channels[i] = 1000 + int32_constrain( channel_out, 0, 1000 );
+					 msg_rc_out.channels[i] = 1000 + int32_constrain( channel_out, 0, 1000 );
 			}
 
-			pub_rc_out_.publish(msg_rc_out_);
+			pub_rc_out_.publish(msg_rc_out);
 		}
 
 		void imuCallback(const sensor_msgs::Imu::ConstPtr& msg_in) {
 			model_imu_ = *msg_in;
+		}
+
+		void forceCompCallback(const mavros_msgs::ActuatorControl::ConstPtr& msg_in) {
+			force_compensation_ = *msg_in;
 		}
 
 		void attitudeTargetCallback( const mavros_msgs::AttitudeTarget::ConstPtr& msg_in ) {
@@ -191,54 +206,29 @@ class ControllerAcro {
 		}
 
 		ControllerAcro() :
-			nh_( "~" ),
+			nh_(),
+			nhp_( "~" ),
 			frame_id_("world"),
 			model_name_("mantis_uav"),
-			topic_attitude_target_("/mantis_uav/command/attitude_target"),
-			topic_imu_("/mantis_uav/imu_data"),
-			topic_rc_out_("/mantis_uav/command/motor_pwm"),
-			pwm_update_rate_(100.0) {
+			pwm_update_rate_(250.0),
+			force_comp_timeout_(0.1),
+			controller_rates_x_( &nhp_, "rates/x"),
+			controller_rates_y_( &nhp_, "rates/y"),
+			controller_rates_z_( &nhp_, "rates/z") {
 
-			sub_attitude_target_ = nh_.subscribe<mavros_msgs::AttitudeTarget>(topic_attitude_target_, 1, &ControllerAcro::attitudeTargetCallback, this);
-			sub_imu_ = nh_.subscribe<sensor_msgs::Imu>(topic_imu_, 10, &ControllerAcro::imuCallback, this);
-			pub_rc_out_ = nh_.advertise<mavros_msgs::RCOut>( topic_rc_out_, 10 );
+			sub_attitude_target_ = nh_.subscribe<mavros_msgs::AttitudeTarget>("command/attitude", 1, &ControllerAcro::attitudeTargetCallback, this);
+			sub_imu_ = nh_.subscribe<sensor_msgs::Imu>("imu_data", 10, &ControllerAcro::imuCallback, this);
+			pub_rc_out_ = nh_.advertise<mavros_msgs::OverrideRCIn>( "command/motor_pwm", 10 );
+			sub_actuator_control_ = nh_.subscribe<mavros_msgs::ActuatorControl>("command/force_compensation", 1, &ControllerAcro::forceCompCallback, this);
 
 			tmr_rc_out_ = nh_.createTimer(ros::Duration(1 / pwm_update_rate_), &ControllerAcro::mixerCallback, this);
 
 			param_ang_r_ff_ = 6.0;
-			double param_pid_rate_x_kp = 0.05;
-			double param_pid_rate_x_ki = 0.0;
-			double param_pid_rate_x_kd = 0.003;
-			double param_pid_rate_x_tau = 0.02;
-
 			param_ang_p_ff_ = 6.0;
-			double param_pid_rate_y_kp = 0.05;
-			double param_pid_rate_y_ki = 0.0;
-			double param_pid_rate_y_kd = 0.003;
-			double param_pid_rate_y_tau = 0.02;
-
 			param_ang_y_ff_ = 2.8;
-			double param_pid_rate_z_kp = 0.2;
-			double param_pid_rate_z_ki = 0.1;
-			double param_pid_rate_z_kd = 0.0;
-			double param_pid_rate_z_tau = 0.02;
-
 			param_rate_r_max_ = 3.85;	//~220 deg/s
 			param_rate_p_max_ = 3.85;	//~220 deg/s
 			param_rate_y_max_ = 1.05;	//~60 deg/s
-			double param_pid_force_x_max = 0.5; //20% throttle
-			double param_pid_force_y_max = 0.5; //20% throttle
-			double param_pid_force_z_max = 0.2; //10% throttle
-
-			controller_rates_x_.setGains( param_pid_rate_x_kp, param_pid_rate_x_ki, param_pid_rate_x_kd, param_pid_rate_x_tau);
-			controller_rates_x_.setOutputMinMax( -param_pid_force_x_max, param_pid_force_x_max );
-			controller_rates_y_.setGains( param_pid_rate_y_kp, param_pid_rate_y_ki, param_pid_rate_y_kd, param_pid_rate_y_tau);
-			controller_rates_y_.setOutputMinMax( -param_pid_force_y_max, param_pid_force_y_max );
-			controller_rates_z_.setGains( param_pid_rate_z_kp, param_pid_rate_z_ki, param_pid_rate_z_kd, param_pid_rate_z_tau);
-			controller_rates_z_.setOutputMinMax( -param_pid_force_z_max, param_pid_force_z_max );
-
-			msg_rc_out_.header.frame_id = frame_id_;
-			msg_rc_out_.channels.resize(NUM_MOTORS);
 
 			ROS_INFO("Listening for outputting motor commands...");
 		}
