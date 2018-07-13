de54aec55542d60ffe7d8741e4882d7d741a9679
Kye Morton <pryre.dev@outlook.com>
2018-07-13 14:37:30 +1000

    Added namespace to header for docs, will break other files

--- mantis_description/include/mantis_description/se_tools.h
+++ mantis_description/include/mantis_description/se_tools.h
@@ -8,146 +8,149 @@
 
 #include <mantis_msgs/BodyInertial.h>
 
-//Math Helpers
-inline Eigen::Matrix3d vee_up(const Eigen::Vector3d& w) {
-	Eigen::Matrix3d W;
+//! The Mantis Description ToolsnNamespace
+namespace MDTools {
+	//Math Helpers
+	inline Eigen::Matrix3d vee_up(const Eigen::Vector3d& w) {
+		Eigen::Matrix3d W;
 
-	W <<  0.0, -w(2),  w(1),
-		 w(2),   0.0, -w(0),
-		-w(1),  w(0),   0.0;
+		W <<  0.0, -w(2),  w(1),
+			 w(2),   0.0, -w(0),
+			-w(1),  w(0),   0.0;
 
-	return W;
-}
-
-inline Eigen::Vector3d vee_down(const Eigen::Matrix3d& W) {
-	return Eigen::Vector3d(W(2,1), W(0,2), W(1,0));
-}
-
-inline Eigen::Matrix<double,6,6> adjoint(const Eigen::Affine3d& g) {
-	Eigen::Matrix<double,6,6> ad;
-	ad << g.linear(), (vee_up(g.translation())*g.linear()),
-		  Eigen::Matrix3d::Zero(),g.linear();
-
-	return ad;
-}
-
-inline Eigen::Matrix<double,6,6> full_inertial(const mantis_msgs::BodyInertial& i) {
-	Eigen::Matrix3d Ir;
-	Ir << i.Ixx, i.Ixy, i.Ixz,
-		  i.Ixy, i.Iyy, i.Iyz,
-		  i.Ixz, i.Iyz, i.Izz;
-
-	Eigen::Matrix<double,6,6> I;
-	I << (i.mass * Eigen::Matrix3d::Identity()), Eigen::Matrix3d::Zero(),
-		 Eigen::Matrix3d::Zero(), Ir;
-
-	return I;
-}
-
-double double_clamp(const double v, const double min, const double max) {
-	return (v < min) ? min : (v > max) ? max : v;
-}
-
-void matrix_clamp(Eigen::MatrixXd &m, const double min, const double max) {
-	for(int i=0; i<m.rows(); i++) {
-		for(int j=0; j<m.cols(); j++) {
-			m(i,j) = double_clamp(m(i,j), min, max);
-		}
-	}
-}
-
-//Matrix Calculation Helpers
-Eigen::Matrix3d extract_yaw_component(const Eigen::Matrix3d &r) {
-	Eigen::Vector3d sp_x = Eigen::Vector3d::UnitX();
-	Eigen::Vector3d sp_y = Eigen::Vector3d::UnitY();
-
-	//As long as y isn't straight up
-	if(r.col(1) != Eigen::Vector3d::UnitZ()) {
-		//If we have ||roll|| > 90Deg
-		Eigen::Vector3d y_c;
-		if(r(2,2) > 0.0) {
-			y_c = r.col(1);
-		} else {
-			y_c = -r.col(1);
-		}
-
-		sp_x = y_c.cross(Eigen::Vector3d::UnitZ());
-		sp_y = Eigen::Vector3d::UnitZ().cross(sp_x);
-	} else { //Use X-axis for the edge-case
-		Eigen::Vector3d x_c = r.col(0);
-
-		sp_y = Eigen::Vector3d::UnitZ().cross(x_c);
-		sp_x = sp_y.cross(Eigen::Vector3d::UnitZ());
+		return W;
 	}
 
-	//Get the pure yaw rotation
-	Eigen::Matrix3d ry;
-	ry << sp_x.normalized(),
-		  sp_y.normalized(),
-		  Eigen::Vector3d::UnitZ();
+	inline Eigen::Vector3d vee_down(const Eigen::Matrix3d& W) {
+		return Eigen::Vector3d(W(2,1), W(0,2), W(1,0));
+	}
 
-	return ry;
-}
+	inline Eigen::Matrix<double,6,6> adjoint(const Eigen::Affine3d& g) {
+		Eigen::Matrix<double,6,6> ad;
+		ad << g.linear(), (vee_up(g.translation())*g.linear()),
+			  Eigen::Matrix3d::Zero(),g.linear();
 
-//Conversion Helpers
-inline Eigen::Vector3d vector_from_msg(const geometry_msgs::Vector3 &v) {
-		return Eigen::Vector3d(v.x, v.y, v.z);
-}
+		return ad;
+	}
 
-inline Eigen::Vector3d point_from_msg(const geometry_msgs::Point &p) {
-		return Eigen::Vector3d(p.x, p.y, p.z);
-}
+	inline Eigen::Matrix<double,6,6> full_inertial(const mantis_msgs::BodyInertial& i) {
+		Eigen::Matrix3d Ir;
+		Ir << i.Ixx, i.Ixy, i.Ixz,
+			  i.Ixy, i.Iyy, i.Iyz,
+			  i.Ixz, i.Iyz, i.Izz;
 
-inline Eigen::Quaterniond quaternion_from_msg(const geometry_msgs::Quaternion &q) {
-		return Eigen::Quaterniond(q.w, q.x, q.y, q.z).normalized();
-}
+		Eigen::Matrix<double,6,6> I;
+		I << (i.mass * Eigen::Matrix3d::Identity()), Eigen::Matrix3d::Zero(),
+			 Eigen::Matrix3d::Zero(), Ir;
 
-inline Eigen::Affine3d affine_from_msg(const geometry_msgs::Pose &pose) {
-		Eigen::Affine3d a;
+		return I;
+	}
 
-		a.translation() << point_from_msg(pose.position);
-		a.linear() << quaternion_from_msg(pose.orientation).toRotationMatrix();
+	double double_clamp(const double v, const double min, const double max) {
+		return (v < min) ? min : (v > max) ? max : v;
+	}
 
-		return a;
-}
+	void matrix_clamp(Eigen::MatrixXd &m, const double min, const double max) {
+		for(int i=0; i<m.rows(); i++) {
+			for(int j=0; j<m.cols(); j++) {
+				m(i,j) = double_clamp(m(i,j), min, max);
+			}
+		}
+	}
 
-inline geometry_msgs::Vector3 vector_from_eig(const Eigen::Vector3d &v) {
-	geometry_msgs::Vector3 vec;
+	//Matrix Calculation Helpers
+	Eigen::Matrix3d extract_yaw_component(const Eigen::Matrix3d &r) {
+		Eigen::Vector3d sp_x = Eigen::Vector3d::UnitX();
+		Eigen::Vector3d sp_y = Eigen::Vector3d::UnitY();
 
-	vec.x = v.x();
-	vec.y = v.y();
-	vec.z = v.z();
+		//As long as y isn't straight up
+		if(r.col(1) != Eigen::Vector3d::UnitZ()) {
+			//If we have ||roll|| > 90Deg
+			Eigen::Vector3d y_c;
+			if(r(2,2) > 0.0) {
+				y_c = r.col(1);
+			} else {
+				y_c = -r.col(1);
+			}
 
-	return vec;
-}
+			sp_x = y_c.cross(Eigen::Vector3d::UnitZ());
+			sp_y = Eigen::Vector3d::UnitZ().cross(sp_x);
+		} else { //Use X-axis for the edge-case
+			Eigen::Vector3d x_c = r.col(0);
 
-inline geometry_msgs::Point point_from_eig(const Eigen::Vector3d &p) {
-	geometry_msgs::Point point;
+			sp_y = Eigen::Vector3d::UnitZ().cross(x_c);
+			sp_x = sp_y.cross(Eigen::Vector3d::UnitZ());
+		}
 
-	point.x = p.x();
-	point.y = p.y();
-	point.z = p.z();
+		//Get the pure yaw rotation
+		Eigen::Matrix3d ry;
+		ry << sp_x.normalized(),
+			  sp_y.normalized(),
+			  Eigen::Vector3d::UnitZ();
 
-	return point;
-}
+		return ry;
+	}
 
-inline geometry_msgs::Quaternion quaternion_from_eig(const Eigen::Quaterniond &q) {
-	geometry_msgs::Quaternion quat;
-	Eigen::Quaterniond qn = q.normalized();
+	//Conversion Helpers
+	inline Eigen::Vector3d vector_from_msg(const geometry_msgs::Vector3 &v) {
+			return Eigen::Vector3d(v.x, v.y, v.z);
+	}
 
-	quat.w = qn.w();
-	quat.x = qn.x();
-	quat.y = qn.y();
-	quat.z = qn.z();
+	inline Eigen::Vector3d point_from_msg(const geometry_msgs::Point &p) {
+			return Eigen::Vector3d(p.x, p.y, p.z);
+	}
 
-	return quat;
-}
+	inline Eigen::Quaterniond quaternion_from_msg(const geometry_msgs::Quaternion &q) {
+			return Eigen::Quaterniond(q.w, q.x, q.y, q.z).normalized();
+	}
 
-inline geometry_msgs::Pose pose_from_eig(const Eigen::Affine3d &g) {
-	geometry_msgs::Pose pose;
+	inline Eigen::Affine3d affine_from_msg(const geometry_msgs::Pose &pose) {
+			Eigen::Affine3d a;
 
-	pose.position = point_from_eig(g.translation());
-	pose.orientation = quaternion_from_eig(Eigen::Quaterniond(g.linear()));
+			a.translation() << point_from_msg(pose.position);
+			a.linear() << quaternion_from_msg(pose.orientation).toRotationMatrix();
 
-	return pose;
+			return a;
+	}
+
+	inline geometry_msgs::Vector3 vector_from_eig(const Eigen::Vector3d &v) {
+		geometry_msgs::Vector3 vec;
+
+		vec.x = v.x();
+		vec.y = v.y();
+		vec.z = v.z();
+
+		return vec;
+	}
+
+	inline geometry_msgs::Point point_from_eig(const Eigen::Vector3d &p) {
+		geometry_msgs::Point point;
+
+		point.x = p.x();
+		point.y = p.y();
+		point.z = p.z();
+
+		return point;
+	}
+
+	inline geometry_msgs::Quaternion quaternion_from_eig(const Eigen::Quaterniond &q) {
+		geometry_msgs::Quaternion quat;
+		Eigen::Quaterniond qn = q.normalized();
+
+		quat.w = qn.w();
+		quat.x = qn.x();
+		quat.y = qn.y();
+		quat.z = qn.z();
+
+		return quat;
+	}
+
+	inline geometry_msgs::Pose pose_from_eig(const Eigen::Affine3d &g) {
+		geometry_msgs::Pose pose;
+
+		pose.position = point_from_eig(g.translation());
+		pose.orientation = quaternion_from_eig(Eigen::Quaterniond(g.linear()));
+
+		return pose;
+	}
 }
