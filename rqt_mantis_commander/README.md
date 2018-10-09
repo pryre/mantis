cec31d9b62a79102450fc1055cf75757cc2997f4
pryre.dev@outlook.com <pryre.dev@outlook.com>
2018-10-09 19:13:15 +1000

    Basic goto panel integrated

---
+++ rqt_mantis_commander/README.md
@@ -0,0 +1,2 @@
+# rqt_mantis_commander
+An RQT interface for issuing goto-type commands to a Mantis UAV
---
+++ rqt_mantis_commander/src/rqt_mantis_commander/commander.py
@@ -0,0 +1,126 @@
+import os
+import math
+import rospkg
+import rospy
+
+from qt_gui.plugin import Plugin
+from python_qt_binding import loadUi
+from python_qt_binding.QtWidgets import QWidget
+
+from mantis_msgs.msg import State#, Parameters
+from geometry_msgs.msg import Pose, Vector3
+
+import actionlib
+from contrail.msg import TrajectoryAction, TrajectoryGoal
+
+class MantisCommander(Plugin):
+	def __init__(self, context):
+		super(MantisCommander, self).__init__(context)
+		# Give QObjects reasonable names
+		self.setObjectName('MantisCommander')
+		rp = rospkg.RosPack()
+
+		# Process standalone plugin command-line arguments
+		#from argparse import ArgumentParser
+		#parser = ArgumentParser()
+		# Add argument(s) to the parser.
+		#parser.add_argument("-q", "--quiet", action="store_true",
+		#              dest="quiet",
+		#              help="Put plugin in silent mode")
+		#args, unknowns = parser.parse_known_args(context.argv())
+		#if not args.quiet:
+		#    print 'arguments: ', args
+		#    print 'unknowns: ', unknowns
+
+		# Create QWidget
+		self._widget = QWidget()
+		# Get path to UI file which is a sibling of this file
+		# in this example the .ui and .py file are in the same folder
+		ui_file = os.path.join(rp.get_path('rqt_mantis_commander'), 'resource', 'MantisCommander.ui')
+		# Extend the widget with all attributes and children from UI file
+		loadUi(ui_file, self._widget)
+		# Give QObjects reasonable names
+		self._widget.setObjectName('MantisCommanderUi')
+		# Show _widget.windowTitle on left-top of each plugin (when
+		# it's set in _widget). This is useful when you open multiple
+		# plugins at once. Also if you open multiple instances of your
+		# plugin at once, these lines add number to make it easy to
+		# tell from pane to pane.ns
+		if context.serial_number() > 1:
+			self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
+		# Add widget to the user interface
+		context.add_widget(self._widget)
+
+		self._widget.button_send_command.clicked.connect(self.button_send_command_pressed)
+		self._widget.button_update_namespaces.clicked.connect(self.button_update_namespaces_pressed)
+
+		self.sub_state = None
+		self.has_state = False
+		self.current_pose = None
+
+		self.update_namespaces()
+
+	def shutdown_plugin(self):
+		if self.sub_state is not None:
+			self.sub_state.unregister()
+
+	def save_settings(self, plugin_settings, instance_settings):
+		instance_settings.set_value('namespace', self.ns)
+		instance_settings.set_value('contrail_prefix', self.contrail_prefix)
+		instance_settings.set_value('joint_prefix', self.joint_prefix)
+
+	def restore_settings(self, plugin_settings, instance_settings):
+		self.ns = instance_settings.value('namespace')
+		self.contrail_prefix = instance_settings.value('contrail_prefix')
+		self.joint_prefix = instance_settings.value('joint_prefix')
+
+	#def trigger_configuration(self):
+		# Comment in to signal that the plugin has a way to configure
+		# This will enable a setting button (gear icon) in each dock widget title bar
+		# Usually used to open a modal configuration dialog
+
+	def button_send_command_pressed(self):
+		rospy.logdebug("Send command pressed!")
+
+		if self.client_base.wait_for_server(rospy.Duration(0.5)):
+			if self.has_state:
+				pos_x = float(self._widget.textbox_goto_pos_x.text())
+				pos_y = float(self._widget.textbox_goto_pos_y.text())
+				pos_z = float(self._widget.textbox_goto_pos_z.text())
+				yaw = float(self._widget.textbox_goto_yaw.text())
+
+				goal_base = TrajectoryGoal()
+				goal_base.duration = rospy.Duration.from_sec(20)
+				goal_base.positions = [Vector3(self.current_pose.position.x,self.current_pose.position.y,self.current_pose.position.z),
+									   Vector3(pos_x,pos_y,pos_z)]
+				goal_base.yaws = [0.0, 0.0]
+
+				goal_base.start = rospy.Time.now() + rospy.Duration.from_sec(2)
+
+				self.client_base.send_goal(goal_base)
+
+				rospy.loginfo("Sending command...")
+
+			else:
+				rospy.logerr("No state information received, can't generate command action!")
+		else:
+			rospy.logerr("Not connected to action server!")
+
+	def button_update_namespaces_pressed(self):
+		self.update_namespaces()
+		rospy.logdebug("Update namespaces button pressed!")
+
+	def callback_state(self, msg_in):
+		self.has_state = True
+		self.current_pose = msg_in.pose
+
+	def update_namespaces(self):
+		self.ns = self._widget.textbox_namespace.text()
+		self.contrail_prefix = self._widget.textbox_contrail.text()
+		self.joint_prefix = self._widget.textbox_joint_prefix.text()
+
+		self.sub_state = rospy.Subscriber(self.ns + "/state", State, self.callback_state)
+
+		self.client_base = actionlib.SimpleActionClient(self.ns + '/' + self.contrail_prefix, TrajectoryAction)
+
+
