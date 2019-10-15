26bdda526da6c2f1a611d9178b0f11a29a48dc87
Kye Morton <pryre.dev@outlook.com>
2019-10-15 17:47:15 +1000

    Updated to use new spline gen

--- mantis_tools/spline_tests/draw_spline_collision.m
+++ mantis_tools/spline_tests/draw_spline_collision.m
@@ -4,51 +4,56 @@ close all;
 clear;
 clc;
 
-set(0,'defaulttextInterpreter','latex');
+set(0,'DefaultTextInterpreter','latex');
+set(0,'DefaultLegendInterpreter','latex');
+set(0,'DefaultAxesXGrid','on');
+set(0,'DefaultAxesYGrid','on');
+set(0,'DefaultAxesZGrid','on');
+% set(0,'DefaultAxesSortMethod','ChildOrder');
+set(0,'DefaultFigureColor','w');
+% set(0, 'DefaultFigureRenderer', 'painter');
 
 
 %%
 
 t0 = 0;
-% dt = 1/125;
-tf = 10;
+dt = 1/200;
+tf = 9;
 
-vpx = [-2,-2,2,2];
-vpy = [0,0.8,1.2,2];
+vpx = [-1,-1,1,1];
+% vpy = [0,0.8,1.2,2];
+vpy = [0,1.5,2.5,4];
 via_est_method = 'fdcc';
 
 
 %%
 num_via_points = length(vpx);
 
-% t = t0:dt:tf;
 t_vias = linspace(t0,tf,num_via_points);
-sd = t_vias(2) - t_vias(1);
 
-viasxu = gen_vias(via_est_method, vpx, sd,'Conditioning','Unconditioned');
-viasyu = gen_vias(via_est_method, vpy, sd,'Conditioning','Unconditioned');
-viasxp = gen_vias(via_est_method, vpx, sd,'Conditioning','Precise');
-viasyp = gen_vias(via_est_method, vpy, sd,'Conditioning','Precise');
+viasxu = gen_vias(via_est_method, vpx, t_vias,'Conditioning','Unconditioned');
+viasyu = gen_vias(via_est_method, vpy, t_vias,'Conditioning','Unconditioned');
+viasxp = gen_vias(via_est_method, vpx, t_vias,'Conditioning','Precise');
+viasyp = gen_vias(via_est_method, vpy, t_vias,'Conditioning','Precise');
 
 % via_steps = ((tf - t0) / dt) / (num_via_points - 1);
 %%
-[t, sxu] = gen_spline(t0, tf, viasxu, 5, 200);
-[~, syu] = gen_spline(t0, tf, viasyu, 5, 200);
-[~, sxp] = gen_spline(t0, tf, viasxp, 5, 200);
-[~, syp] = gen_spline(t0, tf, viasyp, 5, 200);
+[t, sxu] = gen_spline(t_vias, viasxu, 5, dt);
+[~, syu] = gen_spline(t_vias, viasyu, 5, dt);
+[~, sxp] = gen_spline(t_vias, viasxp, 5, dt);
+[~, syp] = gen_spline(t_vias, viasyp, 5, dt);
 
 %%
 
 f1 = figure();
     clf;
-    subplot(2,2,1)
+    subplot(2,1,1)
         hold on;
         plot(t,sxu(1,:), '-b');
         plot(t,sxp(1,:), '-r');
         scatter(t_vias, vpx, 'ro')
         hold off;
         
-        grid on;
 %         xlabel('Time (\(s\))')
 %         ylim([-1.5,0.5])
         ylabel('X Position (\(m\))')
@@ -56,44 +61,42 @@ f1 = figure();
 %         legend('Unconditioned','Precise','Vias')
         legend('Unconditioned','Precise', 'Vias', 'Location', 'SouthEast')
         
-    subplot(2,2,3)
+    subplot(2,1,2)
         hold on;
         plot(t,syu(1,:), '-b');
         plot(t,syp(1,:), '-r');
         scatter(t_vias, vpy, 'ro')
         hold off;
         
-        grid on;
         xlabel('Time (\(s\))')
-        ylim([-0.5,2.5])
+        ylim([-2,6])
         ylabel('Y Position (\(m\))')
 %         legend('Unconditioned','Precise','Vias')
         legend('Unconditioned','Precise', 'Vias', 'Location', 'SouthEast')
- 
-    subplot(1,2,2)
-        hold on;
-        plot(sxu(1,:),syu(1,:), '-b');
-        plot(sxp(1,:),syp(1,:), '-r');
-        scatter(vpx(1,:), vpy(1,:), 'ro')
-        %%
-        obs_color = [0.4 0.4 0.4];
-        rectangle('Position', [-1,0,5,0.8], 'FaceColor',obs_color, 'EdgeColor',obs_color);
-        rectangle('Position', [-4,0,1,2], 'FaceColor',obs_color, 'EdgeColor',obs_color);
-        rectangle('Position', [-3,1.2,4,0.8], 'FaceColor',obs_color, 'EdgeColor',obs_color);
-        rectangle('Position', [3,0,1,2], 'FaceColor',obs_color, 'EdgeColor',obs_color);
-        %%
-        hold off;
-        
-        grid on;
-        xlabel('Time (\(s\))')
-        xlim([-4,4])
-        ylabel('Position (\(m\))')
-        legend('Unconditioned','Precise', 'Vias', 'Location', 'SouthEast')
-        title('\textbf{2D Trajectory through Passage}')
+ %%
+f2 = figure();
+    hold on;
+    plot(sxu(1,:),syu(1,:), '-b');
+    plot(sxp(1,:),syp(1,:), '-r');
+    scatter(vpx(1,:), vpy(1,:), 'ro')
+    
+    obs_color = [0.4 0.4 0.4];
+    rectangle('Position', [-0.5,0,2.5,1.5], 'FaceColor',obs_color, 'EdgeColor',obs_color);
+    rectangle('Position', [-2,0,0.5,2.5], 'FaceColor',obs_color, 'EdgeColor',obs_color);
+    rectangle('Position', [-2,2.5,2.5,1.5], 'FaceColor',obs_color, 'EdgeColor',obs_color);
+    rectangle('Position', [1.5,1.5,0.5,2.5], 'FaceColor',obs_color, 'EdgeColor',obs_color);
+    
+    hold off;
+
+    axis('square')
+    xlabel('X Position (\(m\))')
+    ylabel('Y Position (\(m\))')
+    legend('Unconditioned','Precise', 'Vias', 'Location', 'SouthEast')
+    title('\textbf{2D Trajectory through Passage}')
 
         
 %%
-print(f1, 'spline_collision', '-depsc')
-print(f1, 'spline_collision', '-dpng', '-r720')
+export_fig('./figures/spline_collision_axes', '-eps', f1)
+export_fig('./figures/spline_collision_2d', '-eps', f2)
 
 
